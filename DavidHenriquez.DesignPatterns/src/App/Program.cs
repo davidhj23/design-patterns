374d53669d4c1b83dd20cdfbe4d1e953d1d75bfc
David Henriquez <david.henriquez.23@gmail.com>
2023-12-14 12:16:05 -0500

    ADD Singleton Pattern

---
+++ DavidHenriquez.DesignPatterns/src/App/Program.cs
@@ -0,0 +1,13 @@
+﻿using Core;
+
+var c1 = DbConnection.GetDbConnection();
+var c2 = DbConnection.GetDbConnection();
+
+// Confirm these are the same instance
+if (c1 == c2)
+{
+    Console.WriteLine("Same instance\n");
+}
+
+// Wait for user
+Console.ReadKey();
---
+++ DavidHenriquez.DesignPatterns/src/Core/DbConnection.cs
@@ -0,0 +1,19 @@
+﻿namespace Core;
+
+/// <summary>
+/// The 'Singleton' class
+/// </summary>
+public sealed class DbConnection
+{   
+    private static readonly DbConnection instance = new DbConnection();    
+ 
+    private DbConnection()
+    {   
+        
+    }
+
+    public static DbConnection GetDbConnection()
+    {
+        return instance;
+    }
+}
