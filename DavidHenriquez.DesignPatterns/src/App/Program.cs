97067c201eaf660b417f20e68a6daa845b2b72d7
David Henriquez <david.henriquez.23@gmail.com>
2023-12-14 12:34:38 -0500

    ADD Spectre.Console for pattern selection          New Singleton foler in Core App          Move Dbconnection class to Singleton folder          Swicth statement included for pattern selection

--- DavidHenriquez.DesignPatterns/src/App/Program.cs
+++ DavidHenriquez.DesignPatterns/src/App/Program.cs
@@ -1,12 +1,33 @@
-﻿using Core;
+﻿using Core.Singleton;
+using Spectre.Console;
 
-var c1 = DbConnection.GetDbConnection();
-var c2 = DbConnection.GetDbConnection();
+// Ask for the user's favorite pattern
+var pattern = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+        .Title("What's your [green]favorite pattern[/]?")
+        .PageSize(23)
+        //.MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
+        .AddChoices(new[] {
+            "Singleton"
+        }));
 
-// Confirm these are the same instance
-if (c1 == c2)
+AnsiConsole.WriteLine($"{pattern} example!");
+
+switch (pattern)
 {
-    Console.WriteLine("Same instance\n");
+    case "Singleton":
+        var c1 = DbConnection.GetDbConnection();
+        var c2 = DbConnection.GetDbConnection();
+
+        // Confirm these are the same instance
+        if (c1 == c2)
+        {
+            AnsiConsole.WriteLine("Same instance\n");
+        }        
+        break;
+    default:
+        AnsiConsole.WriteLine("No pattern selected\n");
+        break;
 }
 
 // Wait for user
--- DavidHenriquez.DesignPatterns/src/Core/Singleton/DbConnection.cs
+++ DavidHenriquez.DesignPatterns/src/Core/Singleton/DbConnection.cs
@@ -1,15 +1,15 @@
-﻿namespace Core;
+﻿namespace Core.Singleton;
 
 /// <summary>
 /// The 'Singleton' class
 /// </summary>
 public sealed class DbConnection
-{   
-    private static readonly DbConnection instance = new DbConnection();    
- 
+{
+    private static readonly DbConnection instance = new DbConnection();
+
     private DbConnection()
-    {   
-        
+    {
+
     }
 
     public static DbConnection GetDbConnection()
